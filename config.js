33c0eaa4d538047d822f854cdaaa2958590b5a9e
felipelovis <vasconceloslovison@gmail.com>
2025-11-13 12:04:30 -0300

    Update config.js

--- config.js
+++ config.js
@@ -9,23 +9,21 @@ const PARTICIPANTES = {
     // Adicione mais participantes aqui
 };
 
-// DATAS LIMITE por fase (formato: 'YYYY-MM-DDTHH:MM:SS')
+// DATAS LIMITE por fase (CORRIGIDO PARA 2026!)
 const DATAS_LIMITE = {
-    'Grupo': new Date('2025-06-25T14:00:00'),
-    '16 avos': new Date('2026-11-12T12:00:00'),
-    'Oitavas de final': new Date('2025-11-12T12:00:00'),
-    'Quartas de final': new Date('2025-11-12T16:00:00'),
-    'Semifinais': new Date('2025-11-12T16:00:00'),
-    'Terceiro e Quarto': new Date('2025-11-12T14:00:00'),
-    'Final': new Date('2025-11-12T16:00:00'),
+    'Grupo': new Date('2026-06-11T14:00:00'),
+    '16 avos': new Date('2026-06-28T12:00:00'),
+    'Oitavas de final': new Date('2026-07-02T12:00:00'),
+    'Quartas de final': new Date('2026-07-09T16:00:00'),
+    'Semifinais': new Date('2026-07-14T16:00:00'),
+    'Terceiro e Quarto': new Date('2026-07-18T14:00:00'),
+    'Final': new Date('2026-07-19T16:00:00'),
 };
 
-// ID DO GOOGLE SHEETS (copie da URL da sua planilha)
-// URL: https://docs.google.com/spreadsheets/d/SEU_ID_AQUI/edit
+// ID DO GOOGLE SHEETS
 const SPREADSHEET_ID = '1MjSAQUet51WEfdYbToNn7rJ2faK8t2XQsVlFMX9KSQU';
 
 // CHAVE DA API DO GOOGLE
-// Vou te ensinar a criar depois
 const API_KEY = 'AIzaSyB2SvzIv9lfSfZ3pX8ukjdFDAPL_KOjsCQ';
 
 // ========================================================
