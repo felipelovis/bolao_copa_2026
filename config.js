fa731d5fe44d2e619ed7481ecc29838cf834066e
felipelovis <vasconceloslovison@gmail.com>
2025-11-14 21:33:22 -0300

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 const DATAS_LIMITE = {
     'Grupo': new Date('2026-06-12T14:00:00'),
     '16 avos': new Date('2026-06-12T12:00:00'),
-    'Oitavas de final': new Date('2025-11-12T12:00:00'),
+    'Oitavas de final': new Date('2025-11-15T12:00:00'),
     'Quartas de final': new Date('2025-11-12T16:00:00'),
     'Semifinais': new Date('2025-11-12T16:00:00'),
     'Terceiro e Quarto': new Date('2025-11-12T14:00:00'),
